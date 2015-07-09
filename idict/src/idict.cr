579b3974ed3205bc8bdd59c15a879979baf04823
itang <live.tang@gmail.com>
2015-07-09 22:48:31 +0800

    cleanup

--- idict/src/idict.cr
+++ idict/src/idict.cr
@@ -9,32 +9,33 @@ module Idict
   def t(word)
     url = "http://dict.youdao.com/search?q=#{word}&keyfrom=dict.index"
     resp =  HTTP::Client.get(url)
-    content = resp.body.to_s
-    if content
+    if content = resp.body.to_s
       s = content.index("trans-container")
       s = content.index("<li>", s) if s
       s = s + "<li>".length if s
-
       e = content.index("</li>", s) if s
 
-      if s && e
-        to = content[s...e]
-        begin
-          HTTP::Client.post "http://www.haoshuju.net:3000/dict", body: %({"from":"#{word}", "to": "#{to}"})
-        rescue ex
-          puts ex
-        end
-        to
-      end
+      content[s...e] if s && e
     end
+  end
+
+  def post_to_cloud!(from, to)
+    HTTP::Client.post "http://www.haoshuju.net:3000/dict", body: %({"from":"#{from}", "to": "#{to}"})
   end
 end
 
 word = ARGV[0]? || "hello"
 puts "#{word}:"
 begin
-  Crtang.time do
+  to, _t = Crtang.time do
+    Idict.t(word)
+  end
+  if to
     puts "\t->: #{Idict.t(word)}"
+    puts "try post to cloud..."
+    Idict.post_to_cloud! word, to
+  else
+    puts "NORESP"
   end
 rescue ex: Exception
   puts ex
