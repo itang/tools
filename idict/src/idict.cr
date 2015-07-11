e2206d42a466534471344cae30a039c55c903206
itang <live.tang@gmail.com>
2015-07-11 17:00:09 +0800

    more

--- idict/src/idict.cr
+++ idict/src/idict.cr
@@ -20,22 +20,32 @@ module Idict
   end
 
   def post_to_cloud!(from, to)
-    HTTP::Client.post "http://www.haoshuju.net:3000/dict", body: %({"from":"#{from}", "to": "#{to}"})
+    HTTP::Client.post "http://www.haoshuju.net:8000/dict", body: %({"from":"#{from}", "to": "#{to}"})
+  end
+
+  class Client
+    def run(word: String)
+      begin
+        to, _t = Crtang.time do
+          Idict.t(word).tap {|x| puts "\t->: #{x}" if x }
+        end
+        if to
+          puts "try post to cloud..."
+          Idict.post_to_cloud! word, to
+        else
+          puts "NORESP"
+        end
+      rescue ex: Exception
+        puts ex
+      end
+    end
   end
 end
 
-word = ARGV[0]? || "hello"
-puts "#{word}:"
-begin
-  to, _t = Crtang.time do
-    Idict.t(word).tap {|x| puts "\t->: #{x}" if x }
-  end
-  if to
-    puts "try post to cloud..."
-    Idict.post_to_cloud! word, to
-  else
-    puts "NORESP"
-  end
-rescue ex: Exception
-  puts ex
+word = ARGV[0]?
+unless word
+  puts "Please input the word!"
+else
+  puts "#{word}:"
+  Idict::Client.new.run(word)
 end
