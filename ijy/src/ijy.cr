d5380f8050b2435fda281c15ecacf76431177ef2
itang <live.tang@gmail.com>
2015-07-25 10:08:09 +0800

    cleanup

--- ijy/src/ijy.cr
+++ ijy/src/ijy.cr
@@ -15,28 +15,29 @@ module Ijy
 
   private def browser(link)
     cmd = "xdg-open"
-    system %(#{cmd} "#{link}" > /dev/null 2>&1).tap {|x| puts x }
+    system %(#{cmd} "#{link}").tap {|x| puts x } + " > /dev/null 2>&1"
   end
 
   private def links(config)
     if config
       File.read_lines config
     else
-      ["https://twitter.com/",
-      # "http://www.reddit.com/r/rust",
-      "https://github.com/manastech/crystal",
-      "https://github.com/Codcore/Amethyst",
-      "http://www.reddit.com/r/crystal_programming/",
-      "https://groups.google.com/forum/?fromgroups#!forum/crystal-lang",
-      "https://github.com/stars?direction=desc&sort=updated",
-      "http://getprismatic.com/",
-      "http://www.douban.com",
-      "http://www.weibo.com",
-      "http://www.deftype.com",
-      "http://www.haoshuju.net:8000/",
-      "http://www.adexchanger.cn",
-      "http://www.rtbchina.com/",
-      "http://bbs.szhome.com/30017.html"
+      %w[https://twitter.com/
+        http://www.reddit.com/r/rust
+        https://github.com/manastech/crystal
+        https://github.com/Codcore/Amethyst
+        http://www.reddit.com/r/crystal_programming
+        https://groups.google.com/forum/?fromgroups#!forum/crystal-lang
+        https://github.com/stars?direction=desc&sort=updated
+        https://github.com/notifications
+        http://getprismatic.com
+        http://www.douban.com
+        http://www.weibo.com
+        http://www.deftype.com
+        http://www.haoshuju.net:8000
+        http://www.adexchanger.cn
+        http://www.rtbchina.com
+        http://bbs.szhome.com/30017.html
       ].reverse
     end
   end
