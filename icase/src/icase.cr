906ad7afd109c0445d0b074f20943d21aca9d057
itang <live.tang@gmail.com>
2015-09-02 15:19:24 +0800

    cleanup

--- icase/src/icase.cr
+++ icase/src/icase.cr
@@ -8,7 +8,7 @@ require "./icase/*"
 module Icase
   extend self
 
-  enum Mode
+  enum Action
     Upcase
     Downcase
     Capitalize
@@ -18,52 +18,52 @@ module Icase
     Base64_DE
     PrettyJson
 
-    def self.from_str(m: String?): Icase::Mode
-      case m
+    def self.from_str(action: String?): Action
+      case action
       when "d", "down", "downcase"
-        Icase::Mode::Downcase
+        Downcase
       when "c", "capitalize"
-        Icase::Mode::Capitalize
+        Capitalize
       when "sha1", "sha"
-        Icase::Mode::Sha1
+        Sha1
       when "md5"
-        Icase::Mode::Md5
+        Md5
       when "base64", "+base64"
-        Icase::Mode::Base64_EN
+        Base64_EN
       when "-base64"
-        Icase::Mode::Base64_DE
+        Base64_DE
       when "u", "up", "upcase"
-        Icase::Mode::Upcase
+        Upcase
       when "json"
-        Icase::Mode::PrettyJson
+        PrettyJson
       when nil
-        Icase::Mode::Upcase
+        Upcase
       else
-        raise %(unknow mode "#{m}")
+        raise %(unknow action "#{action}")
       end
     end
   end
 
-  def encode(str: String, mode: Mode): String
-    case mode
-    when Mode::Upcase
+  def encode(str: String, action: Action): String
+    case action
+    when Action::Upcase
       str.upcase
-    when Mode::Downcase
+    when Action::Downcase
       str.downcase
-    when Mode::Capitalize
+    when Action::Capitalize
       str.capitalize
-    when Mode::Sha1
+    when Action::Sha1
       OpenSSL::SHA1.hash(str).map {|x| sprintf("%x", x)}.join("")
-    when Mode::Md5
+    when Action::Md5
       OpenSSL::MD5.hash(str).map {|x| sprintf("%x", x)}.join("")
-    when Mode::Base64_EN
+    when Action::Base64_EN
       Base64.urlsafe_encode(str)
-    when Mode::Base64_DE
+    when Action::Base64_DE
       Base64.decode(str)
-    when Mode::PrettyJson
+    when Action::PrettyJson
       JSON.parse(str).to_pretty_json
     else
-      raise "unknown mode: #{mode}"
+      raise %(unknow action "#{action}")
     end
   end
 end
@@ -78,13 +78,13 @@ end
 def main(argv)
   str = argv[0]?
   if str
-    if %w(--help --h -h).any?{|x| x == str}
+    if %w(--help --h -h).any? {|x| x == str}
       help
     else
-      mode_s = argv[1]?
-      mode = Icase::Mode.from_str(mode_s)
-      ret = Icase.encode(str, mode)
-      puts "#{str} |> #{mode.to_s.downcase.colorize(:red)} =>\n#{ret.colorize(:green)}"
+      action_s = argv[1]?
+      action = Icase::Action.from_str(action_s)
+      ret = Icase.encode(str, action)
+      puts "#{str} |> #{action.to_s.downcase.colorize(:red)} =>\n#{ret.colorize(:green)}"
       puts ""
     end
   else
