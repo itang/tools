1135968820ac03a283d0b921fd22f7a78792bf2c
itang <live.tang@gmail.com>
2015-09-02 11:19:08 +0800

    md5 sha1 base64

--- icase/src/icase.cr
+++ icase/src/icase.cr
@@ -1,19 +1,86 @@
+require "openssl/md5"
+require "openssl/sha1"
+require "base64"
+require "colorize"
 require "./icase/*"
 
 module Icase
-  # TODO Put your code here
+  extend self
+
+  enum Mode
+    Upcase
+    Downcase
+    Capitalize
+    Sha1
+    Md5
+    Base64_EN
+    Base64_DE
+
+    def self.from_str(m: String?): Icase::Mode
+      case m
+      when "d", "down", "downcase"
+        Icase::Mode::Downcase
+      when "c", "capitalize"
+        Icase::Mode::Capitalize
+      when "sha1", "sha"
+        Icase::Mode::Sha1
+      when "md5"
+        Icase::Mode::Md5
+      when "base64", "+base64"
+        Icase::Mode::Base64_EN
+      when "-base64"
+        Icase::Mode::Base64_DE
+      when "u", "up", "upcase"
+        Icase::Mode::Upcase
+      when nil
+        Icase::Mode::Upcase
+      else
+        raise %(unknow mode "#{m}")
+      end
+    end
+  end
+
+  def encode(str: String, mode: Mode): String
+    case mode
+    when Mode::Upcase
+      str.upcase
+    when Mode::Downcase
+      str.downcase
+    when Mode::Capitalize
+      str.capitalize
+    when Mode::Sha1
+      OpenSSL::SHA1.hash(str).map {|x| sprintf("%x", x)}.join("")
+    when Mode::Md5
+      OpenSSL::MD5.hash(str).map {|x| sprintf("%x", x)}.join("")
+    when Mode::Base64_EN
+      Base64.urlsafe_encode(str)
+    when Mode::Base64_DE
+      Base64.decode(str)
+    else
+      raise "unknown mode: #{mode}"
+    end
+  end
 end
 
-str = ARGV[0]? || "hello"
-mode = ARGV[1]?
+################################################################################
+def main()
+  str = ARGV[0]?
+  mode_s = ARGV[1]?
 
-ret = case mode
-when "d"
-  str.downcase
-when "c"
-  str.capitalize
-else
-  str.upcase
+  if !str || %w(--help --h -h).any?{|x|x == str}
+    puts "icase str (upcase|downcase|capitalize|sha1|md5|bas64|-base64)".colorize(:red)
+    return
+  end
+
+  begin
+    mode = Icase::Mode.from_str(mode_s)
+    ret = Icase.encode str.not_nil!, mode
+    puts ""
+    puts "#{str} |> #{mode.to_s.downcase.colorize(:red)} => #{ret.colorize(:green)}"
+    puts ""
+  rescue ex
+    puts ex.message.colorize(:red)
+  end
 end
 
-puts ret
+main()
