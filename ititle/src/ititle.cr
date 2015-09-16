ebc119d31ece670bdcdaf4c0c06a30bfcb0cf732
itang <live.tang@gmail.com>
2015-09-16 22:35:40 +0800

    more case

--- ititle/src/ititle.cr
+++ ititle/src/ititle.cr
@@ -6,8 +6,14 @@ module ITitle
   extend self
   def title(url: String): String?
     resp = HTTP::Client.get(url)
-    title_line = resp.body.lines.find {|x| x.strip.starts_with?("<title>")}
-    title_line.strip[7..-9] if title_line
+    lines = resp.body.lines # ["<!DOCTYPE html>", "<title>", "test", "</title>"]
+    title_line_start = lines.find {|x| x.strip.starts_with?("<title>")}
+
+    if title_line_start && title_line_start.strip.ends_with?("</title>")
+      title_line_start.strip[7..-9]
+    else
+      (lines.take_while {|x| !x.strip.ends_with?("</title>")}).last?.try(&.strip)
+    end
   end
 end
 
