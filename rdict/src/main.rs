d2a6871b23b2f42164b0cbc77c46958d263d0ad3
itang <live.tang@gmail.com>
2015-10-26 22:02:16 +0800

    cleanup

--- rdict/src/main.rs
+++ rdict/src/main.rs
@@ -9,53 +9,41 @@ use hyper::Client;
 use hyper::header::Connection;
 
 fn main() {
-    let word = get_word();
-    if word.is_empty() {
+    if let Some(word) = get_word() {
+        println!("{}:", word);
+        match dict(word) {
+            Some(ret) => println!("\t->: {}", ret),
+            _ => println!("\tUnknown!"),
+        }
+    } else {
         println!("Please input the word.");
-        return;
-    }
-
-    println!("{}:", word);
-    let ret = dict(word);
-    println!("\t->: {}", ret);
-}
-
-fn get_word() -> String {
-    if let Some(arg1) = env::args().nth(1) {
-        arg1
-    }else{
-        "".to_string()
     }
 }
 
-fn dict(word: String) -> String {
+fn get_word() -> Option<String> {
+    env::args().nth(1)
+}
+
+fn dict(word: String) -> Option<String> {
     let url = format!("http://dict.youdao.com/search?q={}&keyfrom=dict.index",
                       word);
     let mut content = http_get_as_string(url);
-    if !content.is_empty() {
-        match content.find("trans-container") {
-            Some(offset) => {
-                content.drain(..offset);
-                match content.find("<li>") {
-                    Some(value1) => {
-                        content.drain(..value1);
-                        let end = content.find("</li>").unwrap();
-                        let start = "</li>".len() - 1;
-                        return content.drain(start..end).collect();
-                    },
-                    _ => (),
-                };
-            },
-            _ => (),
+    if let Some(p1) = content.find("trans-container") {
+        content.drain(..p1);
+        if let Some(p2) = content.find("<li>") {
+            content.drain(..p2);
+            let (end, start) = (content.find("</li>").unwrap(), "</li>".len() - 1);
+            return Some(content.drain(start..end).collect());
         }
     }
-    return "".to_string();
+    return None;
 }
 
 fn http_get_as_string(url: String) -> String {
     let client = Client::new();
 
     // Creating an outgoing request.
+    // &String can automatically coerce to a &str.
     let mut res = client.get(&url)
                         .header(Connection::close())
                         .send()
@@ -66,6 +54,8 @@ fn http_get_as_string(url: String) -> String {
     res.read_to_string(&mut body).unwrap();
     body
 }
+
+/// ////////////////////////////////////////////////////////////////////////////
 
 #[test]
 fn test_dict() {
