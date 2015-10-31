7fa2f1e1b0333339c6dcbcb9b5f9bfa1eb539470
itang <live.tang@gmail.com>
2015-10-31 12:54:24 +0800

    add post to cloud function

--- rdict/src/main.rs
+++ rdict/src/main.rs
@@ -1,18 +1,41 @@
 #![feature(drain)]
 
+// extern crate url;
+extern crate rustc_serialize;
+
+#[macro_use]
 extern crate hyper;
 
 use std::io::Read;
 use std::env;
 
+use rustc_serialize::json;
+// use url::form_urlencoded;
 use hyper::Client;
 use hyper::header::Connection;
+use hyper::header::Headers;
+use hyper::client::Body::BufBody;
+
+header! { (Auth, "Auth") => [String] }
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct RequestBody {
+    to: String,
+    from: String,
+}
 
 fn main() {
     if let Some(word) = get_word() {
         println!("{}:", word);
+        let w = word.clone();
         match dict(word) {
-            Some(ret) => println!("\t->: {}", ret),
+            Some(ret) => {
+                println!("\t->: {}", ret);
+
+                println!("post to cloud...");
+                let resp = post_to_cloud(w, ret);
+                println!("{}", resp);
+            }
             _ => println!("\tUnknown!"),
         }
     } else {
@@ -25,38 +48,72 @@ fn get_word() -> Option<String> {
 }
 
 fn dict(word: String) -> Option<String> {
+    fn http_get_as_string(url: String) -> String {
+        let client = Client::new();
+
+        // Creating an outgoing request.
+        // &String can automatically coerce to a &str.
+        let mut res = client.get(&url)
+                            .header(Connection::close())
+                            .send()
+                            .unwrap();
+
+        // Read the Response.
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+        body
+    }
+
+
+    fn extract_ret(mut content: String) -> Option<String> {
+        if let Some(p1) = content.find("trans-container") {
+            content.drain(..p1);
+            if let Some(p2) = content.find("<li>") {
+                content.drain(..p2);
+                let (end, start) = (content.find("</li>").unwrap(), "</li>".len() - 1);
+                return Some(content.drain(start..end).collect());
+            }
+        }
+        return None;
+    }
+
     let url = format!("http://dict.youdao.com/search?q={}&keyfrom=dict.index",
                       word);
     let content = http_get_as_string(url);
+
     extract_ret(content)
 }
 
-fn extract_ret(mut content: String) -> Option<String> {
-    if let Some(p1) = content.find("trans-container") {
-        content.drain(..p1);
-        if let Some(p2) = content.find("<li>") {
-            content.drain(..p2);
-            let (end, start) = (content.find("</li>").unwrap(), "</li>".len() - 1);
-            return Some(content.drain(start..end).collect());
-        }
-    }
-    return None;
-}
 
-fn http_get_as_string(url: String) -> String {
-    let client = Client::new();
+fn post_to_cloud(from: String, to: String) -> String {
+    let http_post_as_string = |url: String| -> String {
+        let client = Client::new();
 
-    // Creating an outgoing request.
-    // &String can automatically coerce to a &str.
-    let mut res = client.get(&url)
-                        .header(Connection::close())
-                        .send()
-                        .unwrap();
+        let mut headers = Headers::new();
+        headers.set(Connection::close());
+        headers.set(Auth("test;test2015".to_owned()));
 
-    // Read the Response.
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
-    body
+        // let body_str = form_urlencoded::serialize(vec!(("from", from), ("to", to)));
+        let rb = RequestBody {
+            to: to,
+            from: from,
+        };
+        let body_str = json::encode(&rb).unwrap();
+        let bytes = body_str.as_bytes();
+        let length = bytes.len();
+        let mut res = client.post(&url)
+                            .body(BufBody(bytes, length))
+                            .headers(headers)
+                            .send()
+                            .unwrap();
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+        body
+    };
+
+    let ret = http_post_as_string("http://www.haoshuju.net:8000/api/dict".to_string());
+    // let ret = http_post_as_string("http://localhost:8000/api/dict".to_string());
+    ret
 }
 
 /// ////////////////////////////////////////////////////////////////////////////
