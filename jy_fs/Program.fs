719e389b04bffaaef2cb618db90ca9a02f7e279d
itang <live.tang@gmail.com>
2021-08-06 00:05:11 +0800

    multi default paths

--- jy_fs/Program.fs
+++ jy_fs/Program.fs
@@ -1,21 +1,23 @@
 ﻿open Util
 
+
+let fold onSome onNone opt =
+    match opt with
+    | Some it -> onSome it
+    | None -> onNone ()
+
+let openUrls urls =
+    Seq.iteri
+        (fun index url ->
+            printfn $"%d{index} open %s{url}"
+            do openBrowser url)
+        urls
+
 [<EntryPoint>]
 let main argv =
-    let path =
-        pathFromArgv
-            [ @"D:\ProgramData\bin\jiayou.toml"
-              @"/home/itang/bin/jiayou.toml" ]
-            argv
-
-    match path with
-    | Some (x) ->
-        x
-        |> urlsFromTomlPath
-        |> Seq.iteri
-            (fun index url ->
-                printfn $"%d{index} open %s{url}"
-                do openBrowser url)
-    | _ -> printfn $"ERROR: path is invalid."
+    argv
+    |> pathFromArgv [ @"D:\ProgramData\bin\jiayou.toml"
+                      @"/home/itang/bin/jiayou.toml" ]
+    |> fold (urlsFromTomlPath >> openUrls) (fun () -> printfn $"ERROR: path is invalid.")
 
     0 // return an integer exit code
