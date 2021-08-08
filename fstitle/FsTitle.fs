51b98c1ab585177664395283d62c729c9d877ba9
itang <live.tang@gmail.com>
2021-08-08 20:08:07 +0800

    add title fsharp version

---
+++ fstitle/FsTitle.fs
@@ -0,0 +1,20 @@
+﻿module FsTitle
+
+open FsHttp
+open FsHttp.DslCE
+
+let private START_TAG, END_TAG = "<title>", "</title>"
+
+let getBodyFromUrl url = http { GET url } |> Response.toText
+
+let extractTitle (body: string) =
+    let getIndex () =
+        let pos = body.IndexOf(START_TAG)
+        let endPos = body.IndexOf(END_TAG)
+        let start = pos + START_TAG.Length
+        let length = endPos - start
+        (start, length)
+
+    let start, length = getIndex ()
+    let title = body.Substring(start, length).Trim()
+    title
---
+++ fstitle/Program.fs
@@ -0,0 +1,20 @@
+﻿open FsTitle
+
+let VERSION = "0.1.0-20210807"
+
+[<EntryPoint>]
+let main argv =
+    try
+        match argv |> Array.tryHead with
+        | Some url ->
+            let title =
+                url
+                |> getBodyFromUrl
+                |> extractTitle
+
+            printfn $"title: %s{title}"
+        | _ -> printfn "Please input the url."
+    with
+    | e -> printfn $"ERROR: %s{e.Message}"
+
+    0 // return an integer exit code
