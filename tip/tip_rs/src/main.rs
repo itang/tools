d45714ebdd1568faa4f9d58c0ba95ec0d2ad2a55
itang <live.tang@gmail.com>
2022-01-17 20:41:33 +0800

    cleanup

--- tip/tip_rs/src/main.rs
+++ tip/tip_rs/src/main.rs
@@ -1,5 +1,5 @@
-use std::env::args;
-use std::fs::read_to_string;
+use std::env;
+use std::fs;
 use std::path::Path;
 
 use anyhow::Result;
@@ -10,11 +10,12 @@ const VERSION: &'static str = "0.1-20220117";
 fn main() -> Result<()> {
     println!("tip_rs-V{VERSION}");
 
-    let names: Vec<String> = args().skip(1).collect();
+    let names: Vec<String> = env::args().skip(1).collect();
     println!("{names:?}");
 
     for name in names {
-        let content = read_to_string(Path::new(DATA_HOME).join(format!("{name}.md")))?;
+        let path = Path::new(DATA_HOME).join(format!("{name}.md"));
+        let content = fs::read_to_string(path)?;
         println!("{content}");
     }
 
