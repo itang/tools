c7e820c8933a9089dd93018dbca9c5245e4ba1b5
itang <live.tang@gmail.com>
2022-01-30 20:40:09 +0800

    refactor: cleanup

--- tip/tip_scala/main.scala
+++ tip/tip_scala/main.scala
@@ -3,38 +3,47 @@
 //> using javaOpt "-Xmx2g", "-Dfile.encoding=utf8"
 
 import java.nio.file.{Paths, Files}
+import java.nio.charset.StandardCharsets
 import java.io.File
 
 import lib.util.*
-import java.nio.charset.StandardCharsets
 
-@main
-def main(name: String): Unit =
-  println("tip_scala-V0.1-20220130\n")
+object Main:
+  def main(args: Array[String]): Unit =
+    println("tip_scala-V0.1-20220130\n")
 
-  val d = dataDir()
-  val f = d / s"$name.md"
+    args.headOption match
+      case None =>
+        println("Please input name!")
+        println("-" * 80)
+        displayFiles(dataDir())
+      case Some(n) =>
+        doMain(n)
 
-  if File(f).exists then
-    f |>! println
-      |> (Paths.get(_))
-      |> Files.readAllBytes
-      |> (String(_, "UTF-8"))
-      //|>! (Files.writeString(Paths.get("a.txt"), _, StandardCharsets.UTF_8))
-      |> println
-  else displayFiles(d)
+  private def doMain(name: String): Unit =
+    val d = dataDir()
+    val f = d / s"$name.md"
 
-private def dataDir(): String =
-  "TIP_DATA_ROOT".E or ("HOME".E / "bin" / "data" / "tip")
-
-private def displayFiles(d: String): Unit =
-  println("$ tip --help")
-  println("$ tip mvn")
-  File(d).listFiles
-    .sortBy(_.getName)
-    .grouped(6)
-    .foreach(fs =>
-      fs.map(file => f"${file.getName.noFileExtension}%-16s")
-        .mkString(" ")
+    if File(f).exists then
+      f |>! println
+        |> (Paths.get(_))
+        |> Files.readAllBytes
+        |> (String(_, "UTF-8"))
+        //|>! (Files.writeString(Paths.get("a.txt"), _, StandardCharsets.UTF_8))
         |> println
-    )
+    else displayFiles(d)
+
+  private def dataDir(): String =
+    "TIP_DATA_ROOT".E or ("HOME".E / "bin" / "data" / "tip")
+
+  private def displayFiles(d: String): Unit =
+    println("$ tip --help")
+    println("$ tip mvn")
+    File(d).listFiles
+      .sortBy(_.getName)
+      .grouped(6)
+      .foreach(fs =>
+        fs.map(file => f"${file.getName.noFileExtension}%-16s")
+          .mkString(" ")
+          |> println
+      )
