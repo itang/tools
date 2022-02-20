20f570142c5c8afc9f42ad01e988c9eefcacb8b1
itang <live.tang@gmail.com>
2022-02-20 16:34:28 +0800

    more

--- ppath/ppath-rs/src/main.rs
+++ ppath/ppath-rs/src/main.rs
@@ -1,20 +1,31 @@
+use anyhow::Result;
 use std::env;
+use tap::prelude::*;
 
 const FILE_PATH_SEPARATOR_CHAR: char = if cfg!(windows) { ';' } else { ':' };
+const DEFAULT_ENV_NAME: &str = "PATH";
 
-fn main() {
-    match env::var("PATH") {
-        Ok(s) => {
-            let split = s.split(FILE_PATH_SEPARATOR_CHAR);
+fn main() -> Result<()> {
+    /*
+    let name = env::args().nth(1);
+    let name = name
+        .as_ref()
+        .map(String::as_ref)
+        .unwrap_or(DEFAULT_ENV_NAME);
+    */
 
-            let mut vec: Vec<&str> = split.collect();
-            vec.sort_unstable();
+    let name = env::args()
+        .nth(1)
+        .unwrap_or_else(|| DEFAULT_ENV_NAME.to_owned());
 
-            vec.into_iter()
-                .filter(|&x| !x.trim().is_empty())
-                .enumerate()
-                .for_each(|(i, x)| println!("{:2}: {x}", i + 1));
-        }
-        Err(e) => eprintln!("error: {:?}", e),
-    }
+    name.pipe(env::var)?
+        .split(FILE_PATH_SEPARATOR_CHAR)
+        .collect::<Vec<&str>>()
+        .tap_mut(|v| v.sort_unstable())
+        .into_iter()
+        .filter(|&x| !x.trim().is_empty())
+        .enumerate()
+        .for_each(|(i, x)| println!("{:2}: {x}", i + 1));
+
+    Ok(())
 }
