0d9407265e9c2e8131a531d488e0a903d39d0a3e
itang <live.tang@gmail.com>
2022-02-20 14:48:08 +0800

    refactor: split by char

--- ppath/ppath-rs/src/main.rs
+++ ppath/ppath-rs/src/main.rs
@@ -1,20 +1,20 @@
 use std::env;
 
-const FILE_PATH_SEPARATOR: &str = if cfg!(windows) { ";" } else { ":" };
+const FILE_PATH_SEPARATOR_CHAR: char = if cfg!(windows) { ';' } else { ':' };
 
 fn main() {
     match env::var("PATH") {
         Ok(s) => {
-            let split = s.split(FILE_PATH_SEPARATOR);
+            let split = s.split(FILE_PATH_SEPARATOR_CHAR);
 
             let mut vec: Vec<&str> = split.collect();
             vec.sort_unstable();
 
             vec.into_iter()
-                .filter(|&x| x.trim() != "")
+                .filter(|&x| !x.trim().is_empty())
                 .enumerate()
-                .for_each(|(i, x)| println!("{:3}: {x}", i + 1));
+                .for_each(|(i, x)| println!("{:2}: {x}", i + 1));
         }
-        Err(e) => println!("error: {:?}", e),
+        Err(e) => eprintln!("error: {:?}", e),
     }
 }
