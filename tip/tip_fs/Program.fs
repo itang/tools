9b668f64579077210c7b75b5acb99a16e29532b2
itang <live.tang@gmail.com>
2022-06-16 21:31:26 +0800

    .net 7

--- tip/tip_fs/Program.fs
+++ tip/tip_fs/Program.fs
@@ -3,7 +3,7 @@ open Util
 open Tip
 
 [<Literal>]
-let version = "0.2-20220211"
+let version = "0.2-20220616"
 
 [<EntryPoint>]
 let main argv =
--- tip/tip_fs/tasks.py
+++ tip/tip_fs/tasks.py
@@ -33,16 +33,17 @@ def _is_windows():
 @task
 def publish(c):
     if _is_windows():
-        c.run('dotnet publish -r win-x64 -c Release --self-contained /p:PublishSingleFile=true')
-        #c.run('dotnet publish -r win-x64 -c Release --self-contained /p:PublishSingleFile=true /p:PublishTrimmed=true')
+        #c.run('dotnet publish -r win-x64 -c Release')
+        #c.run('dotnet publish -r win-x64 -c Release --self-contained /p:PublishSingleFile=true')
+        c.run('dotnet publish -r win-x64 -c Release --self-contained /p:PublishSingleFile=true /p:PublishTrimmed=true')
     else:
-        c.run('dotnet publish -r linux-x64 -c Release --self-contained -p:PublishSingleFile=true')
-        #c.run('dotnet publish -r linux-x64 -c Release --self-contained -p:PublishSingleFile=true -p:PublishTrimmed=true')
+        #c.run('dotnet publish -r linux-x64 -c Release')
+        c.run('dotnet publish -r linux-x64 -c Release --self-contained -p:PublishSingleFile=true -p:PublishTrimmed=true')
 
 
 @task(publish)
 def install(c):
     if _is_windows():
-        c.run('coreutils cp bin\\Release\\net6.0\\win-x64\\publish\\tip.exe D:/dev-env/bin/tip_fs.exe')
+        c.run('coreutils cp bin\\Release\\net7.0\\win-x64\\publish\\tip.exe D:/dev-env/bin/tip_fs.exe')
     else:
-        c.run('cp bin/Release/net6.0/linux-x64/publish/tip ~/.local/bin/tip_fs')
+        c.run('cp bin/Release/net7.0/linux-x64/publish/tip ~/.local/bin/tip_fs')
