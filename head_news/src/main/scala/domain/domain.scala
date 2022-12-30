0d3be2caa12c96e2982e768097287f2005e5dbc2
itang <live.tang@gmail.com>
2022-12-30 15:13:21 +0800

    chore: cleanup

--- head_news/src/main/scala/domain/domain.scala
+++ head_news/src/main/scala/domain/domain.scala
@@ -29,14 +29,9 @@ object SinaNews extends HeadNewsProvider:
       text <- resp.text()
     yield
       val start = text.indexOf(startTag) + startTag.length()
-      val end = text.indexOf("</a>", start)
-      val line = text.substring(start, end).trim()
+      val end   = text.indexOf("</a>", start)
+      val line  = text.substring(start, end).trim()
 
       line match
         case R(href, title) => Some(HeadNews(title, href))
-        case _ => None
-
-/*
-val s =
-  """<h1 data-client="headline"><a target="_blank" href="https://news.sina.com.cn/c/xl/2022-11-16/doc-imqmmthc4760493.shtml" class="linkNewsTopBold">G2峰会第一天 习主席重点谈了什么？</a></h1>"""
- */
+        case _              => None
