d8f472fba27bf0158626a20ac7752eadb014b3c3
itang <live.tang@gmail.com>
2023-02-17 14:45:38 +0800

    refactor: lib package

--- calendar/calendar-rs/src/main.rs
+++ calendar/calendar-rs/src/main.rs
@@ -1,11 +1,4 @@
-use std::cmp::min;
-
-use chrono::{Datelike, Days, offset, Weekday};
 use clap::Parser;
-
-//TODO: 月日历
-//TODO: 周日历
-//TODO: 年日历
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -14,41 +7,10 @@ struct Args {
     days: u64,
 }
 
-const MAX_DAYS: u64 = 10_000;
-
 fn main() {
     let args = Args::parse();
 
-    let now = offset::Local::now();
+    let output = calendar::display_day(args.days);
 
-    let days = min(args.days, MAX_DAYS);
-    let lines: Vec<String> = (0..=days).map(|i| {
-        let to = now.checked_add_days(Days::new(i)).unwrap();
-        let df: &str = &to.format("%Y-%m-%d").to_string();
-        let wf = to.weekday().format().unwrap_or("");
-        format!("{i}: {df} - {wf}")
-    }).collect();
-    let output = lines.join("\n");
     println!("{output}");
-}
-
-trait WS {
-    type Output;
-    fn format(&self) -> Self::Output;
-}
-
-impl WS for Weekday {
-    type Output = Option<&'static str>;
-    fn format(&self) -> Self::Output {
-        match self.num_days_from_monday() {
-            0 => Some("星期一"),
-            1 => Some("星期二"),
-            2 => Some("星期三"),
-            3 => Some("星期四"),
-            4 => Some("星期五"),
-            5 => Some("星期六"),
-            6 => Some("星期日"),
-            _ => None,
-        }
-    }
 }
