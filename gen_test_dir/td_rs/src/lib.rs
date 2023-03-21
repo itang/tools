8a7a60a0cd10ade496a34b452f58ce84b2d21eea
itang <live.tang@gmail.com>
2023-03-21 09:53:55 +0800

    chore: u

--- gen_test_dir/td_rs/src/lib.rs
+++ gen_test_dir/td_rs/src/lib.rs
@@ -1,18 +1,31 @@
+#![deny(clippy::unwrap_used)]
+#![forbid(unsafe_code)]
+#![deny(missing_docs)]
+
+//! td lib
+
 use std::fs;
 
 use chrono::prelude::*;
 
+/// generate a dir.
 pub fn gen_dir_str() -> String {
     let now = Local::now();
     let s = format!("{}", now.format("%m%d"));
-    if s.starts_with("0") {
-        s[1..].to_string()
+    if s.starts_with('0') {
+        match s.strip_prefix('0') {
+            Some(r) => r.to_string(),
+            None => unreachable!(),
+        }
+        //s[1..].to_string()
     } else {
         s
     }
 }
 
+/// DirCreate
 pub trait DirCreate {
+    /// create dir from self
     fn create(&self) -> bool;
 }
 
