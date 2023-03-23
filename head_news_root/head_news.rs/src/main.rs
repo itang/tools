a7dbe89689f6a8d1bec9ae096bd4deb6f6c06dc1
itang <live.tang@gmail.com>
2023-03-23 23:00:11 +0800

    fix: tokio runtime enable io

--- head_news_root/head_news.rs/src/main.rs
+++ head_news_root/head_news.rs/src/main.rs
@@ -4,6 +4,8 @@ use tokio::runtime;
 use head_news::*;
 
 // https://stackoverflow.com/questions/66960947/what-is-the-smallest-feature-set-to-enable-polling-a-future-with-tokio
+//#[tokio::main(flavor = "current_thread")]
+/*async*/
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let news_list = runtime::Builder::new_current_thread()
         .enable_io()
