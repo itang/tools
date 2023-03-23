12462c5c84915e252c8b9400d1f11d2f0412216b
itang <live.tang@gmail.com>
2023-03-23 20:01:09 +0800

    chore: simply use tokio

--- head_news_root/head_news.rs/src/main.rs
+++ head_news_root/head_news.rs/src/main.rs
@@ -1,11 +1,15 @@
 use tabled::{Style, TableIteratorExt};
+use tokio::runtime;
 
 use head_news::*;
 
 // https://stackoverflow.com/questions/66960947/what-is-the-smallest-feature-set-to-enable-polling-a-future-with-tokio
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let news_list = Sina::get_news().await?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let news_list = runtime::Builder::new_current_thread()
+        .build()
+        .expect("build tokio runtime")
+        .block_on(Sina::get_news())?;
+    // let news_list = Sina::get_news().await?;
 
     let mut table = news_list.table();
     table.with(Style::psql());
