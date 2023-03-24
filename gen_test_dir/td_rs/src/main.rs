abe0a1445186fbb3fdd2f142c50053c822f2686b
itang <live.tang@gmail.com>
2023-03-24 16:53:37 +0800

    chore: u

--- gen_test_dir/td_rs/src/main.rs
+++ gen_test_dir/td_rs/src/main.rs
@@ -1,23 +1,42 @@
 use std::{env::current_dir, process::ExitCode};
 
+use colored::Colorize;
+
 use td::gen_dir_str;
 use td::DirCreate;
 
+macro_rules! info {
+    ($($arg:tt)*) => {{
+        let res = std::fmt::format(std::format_args!($($arg)*));
+         println!("{}: {}", "INFO".blue(), res.green());
+    }}
+}
+
+macro_rules! warn {
+      ($($arg:tt)*) => {{
+        let res = std::fmt::format(std::format_args!($($arg)*));
+           println!("{}: {}", "WARN".yellow(), res.red());
+    }}
+}
+
 fn main() -> ExitCode {
     let dir = gen_dir_str();
-    let r = dir.create();
+    let create_result = dir.create();
 
-    println!("try to create directory: '{dir}' on {:?}", current_dir().expect("get current dir"));
+    info!("try to create directory: '{dir}' on '{:?}'...", current_dir().expect("get current dir"));
 
-    match r {
+    let exit_code = match create_result {
         Ok(()) => {
-            println!("create {dir} success");
-            println!("cd {dir}");
+            info!("create '{dir}' directory success");
             ExitCode::SUCCESS
         },
         Err(err) => {
-            println!("{err}");
+            warn!("{err}");
             ExitCode::FAILURE
         },
-    }
+    };
+
+    info!("cd {dir}");
+
+    exit_code
 }
