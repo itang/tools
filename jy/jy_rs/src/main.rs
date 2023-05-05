894007724f7166995be0d426db4b29015ea751af
itang <live.tang@gmail.com>
2023-05-05 15:25:31 +0800

    chore: u

--- jy/jy_rs/src/main.rs
+++ jy/jy_rs/src/main.rs
@@ -1,8 +1,14 @@
+//! jy_rs.
+//!
+//! jy cli rust version.
+#![deny(clippy::unwrap_used)]
+#![forbid(unsafe_code)]
+#![deny(missing_docs)]
+
 use std::{fs, path::PathBuf};
-//use std::process::{Command, Stdio};
 
 use anyhow::Result;
-use structopt::StructOpt;
+use clap::Parser;
 use toml::{self, Value};
 
 use opt::{IConfigPath, Opt};
@@ -10,7 +16,7 @@ use opt::{IConfigPath, Opt};
 mod opt;
 
 fn main() -> Result<()> {
-    let opt = Opt::from_args();
+    let opt = Opt::parse();
     println!("{opt:?}");
 
     match opt {
@@ -21,13 +27,31 @@ fn main() -> Result<()> {
             println!("{}", "-".repeat(80));
             println!("dry run. exit!");
             Ok(())
-        }
+        },
+        Opt { show_info: true, .. } => {
+            println!("CONFIG_PATH_ENV_KEY: {}", opt::CONFIG_PATH_ENV_KEY);
+            println!("ENV {} VALUE: {:?}", opt::CONFIG_PATH_ENV_KEY, std::env::var(opt::CONFIG_PATH_ENV_KEY));
+            println!("DEFAULT FILE NAME: {}", opt::DEFAULT_FILE_NAME);
+            println!("DEFAULT CONFIG CONTENT:\n{}", opt::DEFAULT_CONFIG);
+
+            Ok(())
+        },
         _ => {
-            let content = get_content(opt.get_config_path()?)?;
+            let content = match opt.get_config_path() {
+                Ok(path) => match get_content(path.clone()) {
+                    Ok(content) => content,
+                    Err(e) => panic!("WARN: 无法加载配置文件 {:?}, error: {}", path, e),
+                },
+                Err(e) => {
+                    println!("WARN: 未指定要加载配置文件, 使用默认配置列表 (error: {})", e);
+                    opt::DEFAULT_CONFIG.into()
+                },
+            };
+
             let config = content.parse::<Value>()?;
 
             browser_batch(config)
-        }
+        },
     }
 }
 
@@ -38,18 +62,15 @@ fn get_content(config_path: PathBuf) -> Result<String> {
 }
 
 fn browser_batch(config: Value) -> Result<()> {
-    let urls = config["urls"]
-        .as_array()
-        .into_iter()
-        .flat_map(|x| x.iter().flat_map(|y| y.as_str().into_iter()));
+    let urls = config["urls"].as_array().into_iter().flat_map(|x| x.iter().flat_map(|y| y.as_str().into_iter()));
     for (index, url) in urls.enumerate() {
         println!("{index:4}: open {url}", index = index + 1);
-        let _ = browser_single_url(url)?;
+        browser_single_url(url)?;
     }
 
     Ok(())
 }
 
-fn browser_single_url(url: &str) -> Result<std::process::Output> {
+fn browser_single_url(url: &str) -> Result<()> {
     Ok(webbrowser::open(url)?)
 }
--- jy/jy_rs/src/opt.rs
+++ jy/jy_rs/src/opt.rs
@@ -2,37 +2,48 @@ use std::env;
 use std::path::{Path, PathBuf};
 
 use anyhow::Result;
-use structopt::StructOpt;
+use clap::Parser;
 
 pub trait IConfigPath {
     fn get_config_path(&self) -> Result<PathBuf>;
 }
 
-#[derive(StructOpt, Debug)]
-#[structopt(name = "jy_rs")]
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
 pub struct Opt {
-    #[structopt(short, long, parse(from_os_str))]
+    /// The config file
+    #[arg(short, long)]
     config: Option<PathBuf>,
-    // The number of occurrences of the `v/verbose` flag
-    //// Verbose mode (-v, -vv, -vvv, etc.)
-    //#[structopt(short, long, parse(from_occurrences))]
-    //verbose: u8,
-    #[structopt(short, long)]
+
+    /// Dry run mode
+    #[arg(short, long)]
     pub dry_run: bool,
+
+    /// Show info
+    #[arg(short, long)]
+    pub show_info: bool,
 }
 
-pub static DEFAULT_FILE_NAME: &str = "jiayou.toml";
+macro_rules! default_config_name {
+    () => {
+        "jiayou.toml"
+    };
+}
+
+pub static DEFAULT_FILE_NAME: &str = default_config_name!();
+
+pub static CONFIG_PATH_ENV_KEY: &str = "JY_CONFIG";
+
+pub static DEFAULT_CONFIG: &str = include_str!(default_config_name!());
 
 impl IConfigPath for Opt {
     fn get_config_path(&self) -> Result<PathBuf> {
         match &self.config {
             Some(p) => Ok(p.clone()),
-            None => env::var("JY_CONFIG")
+            None => env::var(CONFIG_PATH_ENV_KEY)
                 .map(|c| Path::new(&c).to_path_buf())
-                .or_else(|_| {
-                    Ok(env::var("HOME")
-                        .map(|x| Path::new(&x).join("bin").join(DEFAULT_FILE_NAME))?)
-                }),
+                .or_else(|_| Ok(env::var("HOME").map(|x| Path::new(&x).join("bin").join(DEFAULT_FILE_NAME))?)),
         }
     }
 }
