440ecff79f8dcc76dc4628d16c35e4a9682cade4
itang <live.tang@gmail.com>
2023-05-07 11:06:49 +0800

    chore: u

--- jy/jy_rs/src/main.rs
+++ jy/jy_rs/src/main.rs
@@ -8,7 +8,7 @@
 use std::fs;
 use toml::{self, Value};
 
-use anyhow::Result;
+use anyhow::{Error, Result};
 use clap::Parser;
 
 use jy::{
@@ -21,50 +21,56 @@ fn main() -> Result<()> {
     println!("{opt:?}");
 
     match opt {
-        Opt { dry_run: true, .. } => {
-            let content = jy::get_content(opt.get_config_path()?)?;
-            println!("{content}");
-
-            println!("{}", "-".repeat(80));
-            println!("dry run. exit!");
-            Ok(())
-        },
-        Opt { show_info: true, .. } => {
-            println!("CONFIG_PATH_ENV_KEY: {}", opt::CONFIG_PATH_ENV_KEY);
-            println!("ENV {} VALUE: {:?}", opt::CONFIG_PATH_ENV_KEY, std::env::var(opt::CONFIG_PATH_ENV_KEY));
-            println!("DEFAULT FILE NAME: {}", opt::DEFAULT_FILE_NAME);
-            println!("DEFAULT CONFIG CONTENT:\n{}", opt::DEFAULT_CONFIG);
-
-            Ok(())
-        },
-        _ => {
-            let content = match opt.get_config_path() {
-                Ok(path) => {
-                    println!("INFO: 配置路径:{:?}", path.clone());
-                    match jy::get_content(path.clone()) {
-                        Ok(content) => content,
-                        Err(e) => panic!("WARN: 尝试从配置路径加载文件失败 {:?}, error: {}", path, e),
-                    }
-                },
-                Err(_) => {
-                    let home_config_dir = dirs::home_dir().expect("get home dir").join(".jy");
-                    println!("INFO: 未指定要加载配置文件, 尝试从{:?}加载", home_config_dir.clone());
-                    let home_config_path = home_config_dir.join(opt::DEFAULT_FILE_NAME);
-                    if home_config_path.exists() {
-                        println!("INFO: 默认配置文件存在 {:?}", home_config_path.clone());
-                        jy::get_content(home_config_path)?
-                    } else {
-                        fs::create_dir(home_config_dir).expect("create dir");
-                        println!("WARN: 默认配置文件不存在, 使用默认配置列表创建{:?}...", home_config_path.clone());
-                        fs::write(home_config_path, opt::DEFAULT_CONFIG).expect("write file");
-                        opt::DEFAULT_CONFIG.into()
-                    }
-                },
-            };
-
-            let config = content.parse::<Value>()?;
-
-            browser::browser_batch(config)
-        },
+        Opt { dry_run: true, .. } => handle_dry_run(&opt),
+        Opt { show_info: true, .. } => handle_show_info(),
+        _ => handle_jy(opt),
     }
+}
+
+fn handle_jy(opt: Opt) -> Result<()> {
+    let content = match opt.get_config_path() {
+        Ok(path) => {
+            println!("INFO: 配置路径:{:?}", path.clone());
+            match jy::get_content(path.clone()) {
+                Ok(content) => content,
+                Err(e) => panic!("WARN: 尝试从配置路径加载文件失败 {:?}, error: {}", path, e),
+            }
+        },
+        Err(_) => {
+            let home_config_dir = dirs::home_dir().expect("get home dir").join(".jy");
+            println!("INFO: 未指定要加载配置文件, 尝试从{:?}加载", home_config_dir.clone());
+            let home_config_path = home_config_dir.join(opt::DEFAULT_FILE_NAME);
+            if home_config_path.exists() {
+                println!("INFO: 默认配置文件存在 {:?}", home_config_path.clone());
+                jy::get_content(home_config_path)?
+            } else {
+                fs::create_dir(home_config_dir).expect("create dir");
+                println!("WARN: 默认配置文件不存在, 使用默认配置列表创建{:?}...", home_config_path.clone());
+                fs::write(home_config_path, opt::DEFAULT_CONFIG).expect("write file");
+                opt::DEFAULT_CONFIG.into()
+            }
+        },
+    };
+
+    let config = content.parse::<Value>()?;
+
+    browser::browser_batch(config)
+}
+
+fn handle_show_info() -> Result<(), Error> {
+    println!("CONFIG_PATH_ENV_KEY: {}", opt::CONFIG_PATH_ENV_KEY);
+    println!("ENV {} VALUE: {:?}", opt::CONFIG_PATH_ENV_KEY, std::env::var(opt::CONFIG_PATH_ENV_KEY));
+    println!("DEFAULT FILE NAME: {}", opt::DEFAULT_FILE_NAME);
+    println!("DEFAULT CONFIG CONTENT:\n{}", opt::DEFAULT_CONFIG);
+
+    Ok(())
+}
+
+fn handle_dry_run(opt: &Opt) -> Result<(), Error> {
+    let content = jy::get_content(opt.get_config_path()?)?;
+    println!("{content}");
+
+    println!("{}", "-".repeat(80));
+    println!("dry run. exit!");
+    Ok(())
 }
