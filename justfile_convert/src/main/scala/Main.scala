4caab45f715ac4ff6f6eeea0a451f954b877f8b3
itang <live.tang@gmail.com>
2023-06-29 00:17:28 +0800

    chore: u

--- justfile_convert/src/main/scala/Main.scala
+++ justfile_convert/src/main/scala/Main.scala
@@ -1,8 +1,6 @@
 import tang.*
 import scala.language.unsafeNulls
 
-val ROOT_DIR = "D:/workspace/donew_rs/templates"
-
 import upickle.default.*
 
 case class DenoConfig(tasks: Map[String, String])
@@ -12,61 +10,72 @@ object DenoConfig:
 
 object Main:
   def main(args: Array[String]): Unit = time {
-    val path = os.Path(ROOT_DIR)
-    os.walk(path).foreach: path =>
-      val file = path.toIO
-      val name = file.getName
-      if file.isFile && name.startsWith("deno.json") then
-        println(path)
-
+    args.headOption.getOrElse(tang.panic_!("input dir"))
+      |> getDenoJsonFiles
+      |> (it => if it.isEmpty then tang.panic_!("未找到deno.json文件") else it)
+      |> (_.foreach: path =>
         try
-          val justfileContent = convertToJustfile(file) |>! println
-          os.write.over(os.Path(file.getParentFile) / "justfile", justfileContent)
-        catch
-          case e: Exception => e.printStackTrace()
-
+          val justfileContent = convertToJustfile(path) |>! println
+          os.write.over(os.Path(path.toIO.getParentFile) / "justfile", justfileContent)
+        catch case e: Exception => e.printStackTrace())
   }.ignore()
 
+private def getDenoJsonFiles(dir: String): List[os.Path] = {
+  val path = os.Path(java.io.File(dir).getAbsoluteFile)
+  os
+    .walk(path)
+    .filter { path =>
+      val file = path.toIO
+      file.isFile && file.getName.startsWith("deno.json")
+    }
+    .toList
+}
 
-def convertToJustfile(file: java.io.File): String =
-  val sb = StringBuilder()
-  val iter = os.read(os.Path(file)).linesIterator
-    .map(_.trim)
-    .filter(!_.isBlank)
-    .filter(it => !(it.startsWith("//") || it.startsWith("#")))
-    .toList.iterator
+object convertToJustfile {
+  def apply(path: os.Path): String = path |> toJsonString |> jsonToJustfile
 
+  private def jsonToJustfile(json: String): String = {
+    val denoConfig = read[DenoConfig](json)
+    val j = denoConfig.tasks
+      .map { case (k, v) =>
+        val key   = k.replaceAll(":", "_")
+        val value = v.replaceAll("&&", ";")
+        s"$key:\n  $value\n"
+      }
+      .mkString("\n")
 
-  while iter.hasNext do
-    val curr = iter.next()
-    if iter.hasNext then
-      val next = iter.next()
-      if next == "}" then
-        sb.append(curr.replace(",", "")).ignore()
-      else
-        sb.append(curr).ignore()
-      sb.append("\n").append(next).append('\n').ignore()
-    else
-      sb.append(curr).append('\n').ignore()
+    s"""|set shell := ["nu", "-c"]
+        |
+        |default:
+        |  just --list
+        |
+        |$j
+        |""".stripMargin
+  }
 
+  private def toJsonString(path: os.Path): String = {
+    if path.toIO.getName.endsWith(".json") then os.read(path)
+    else {
+      val iter = os
+        .read(path)
+        .linesIterator
+        .map(_.trim)
+        .filter(!_.isBlank)
+        .filter(it => !(it.startsWith("//") || it.startsWith("#")))
+        .toList
+        .iterator
 
-  sb |>! println
+      val sb = StringBuilder()
+      while iter.hasNext do
+        val curr = iter.next()
+        if iter.hasNext then
+          val next = iter.next()
+          if next == "}" then sb.append(curr.replace(",", "")).ignore()
+          else sb.append(curr).ignore()
+          sb.append("\n").append(next).append('\n').ignore()
+        else sb.append(curr).append('\n').ignore()
 
-  println("-" * 100)
-
-  val denoConfig = read[DenoConfig](sb.toString())
-  denoConfig.toString
-  val j = denoConfig.tasks.map { case (k, v) =>
-    val key = k.replaceAll(":", "_")
-    val value = v.replaceAll("&&", ";")
-    s"$key:\n  $value\n"
-  }.mkString("\n")
-
-  s"""|set shell := ["nu", "-c"]
-      |
-      |default:
-      |  just --list
-      |
-      |$j
-      |""".stripMargin
-
+      sb.toString()
+    }
+  }
+}
