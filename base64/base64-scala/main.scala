0fe725ab92f3284b103effc459f1b4843fef0d04
itang <live.tang@gmail.com>
2023-10-30 12:17:53 +0800

    chore: u

--- base64/base64-scala/main.scala
+++ base64/base64-scala/main.scala
@@ -1,9 +1,10 @@
 //> using scala "3.3.1"
 //> using option "-Wunused:all" "-Wvalue-discard"  "-Wnonunit-statement"
-//> using nativeGc none
-////> using nativeMode release-fast
+//> using nativeGc "none"
+//> using nativeMode "release-fast"
+//> using nativeLto "thin"
 //> using lib "com.funpaas::tang-scala:0.1.2-SNAPSHOT"
-//> using toolkit latest
+////> using toolkit latest
 
 //> using javaOpt "-Dfile.encoding=utf-8"
 
