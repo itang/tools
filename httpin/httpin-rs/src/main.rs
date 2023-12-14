2695411022aaed41ab6bd5da58cd1e86aeae9890
itang <live.tang@gmail.com>
2023-12-14 14:33:33 +0800

    chore: u

--- httpin/httpin-rs/src/main.rs
+++ httpin/httpin-rs/src/main.rs
@@ -1,5 +1,5 @@
 #![deny(clippy::unwrap_used)]
-//#![forbid(unsafe_code)]
+#![forbid(unsafe_code)]
 
 use axum::{
     extract::Request,
@@ -8,6 +8,7 @@ use axum::{
     routing::any,
     Router,
 };
+use std::borrow::Cow;
 use std::error::Error;
 
 use http_body_util::BodyExt;
@@ -34,7 +35,10 @@ async fn handler(request: Request) -> Result<String, Response> {
         .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()).into_response())?
         .to_bytes();
 
-    let request_body = unsafe { String::from_utf8_unchecked(bytes.to_vec()) };
+    let request_body = match String::from_utf8_lossy(&bytes) {
+        Cow::Borrowed(s) => s.to_string(),
+        Cow::Owned(s) => s, //hex::encode(bytes),
+    };
 
     let text = format!("{first_line}\n\n{headers}\n\n{request_body}");
 
