ce5c0c1b0b1a95ecdf09bcf13876be3535abd4a6
itang <live.tang@gmail.com>
2024-01-29 23:16:21 +0800

    chore: u

--- icoder/icoder-fs/Program.fs
+++ icoder/icoder-fs/Program.fs
@@ -18,13 +18,14 @@ let main argv =
     | [| "-h" |] -> print_help ()
     | [| "--version" |]
     | [| "-v" |] -> printfn "v0.1-20240129.1"
-    | [| "base64"; input |] -> Encoding.UTF8.GetBytes(input) |> Convert.ToBase64String |> printfn "%s"
+    | [| "base64"; input |] -> input |> Encoding.UTF8.GetBytes |> Convert.ToBase64String |> printfn "%s"
     | [| "base64"; "-d"; input |] -> input |> Convert.FromBase64String |> Encoding.UTF8.GetString |> printfn "%s"
     | [| "uuid" |] -> Guid.NewGuid().ToString() |> printfn "%s"
-    | v ->
-        let command = v |> Array.tryHead |> Option.defaultValue ""
-        printfn $"unknown command '{command}'"
+    | _ ->
+        let command = argv |> Array.tryHead |> Option.defaultValue ""
+        printfn $"Unknown command '{command}'"
         "*" |> String.replicate 50 |> printfn "%s"
+
         print_help ()
 
     0
