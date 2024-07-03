2c70cee4f0fcd4218cd12e01fdd6b163a859509a
itang <live.tang@gmail.com>
2024-07-03 16:32:29 +0800

    chore: u

--- jy/jy_rs/src/lib/infrastructure.rs
+++ jy/jy_rs/src/lib/infrastructure.rs
@@ -12,7 +12,11 @@ pub struct BrowserImpl;
 
 impl Browser for BrowserImpl {
     fn browser_single_url(&self, url: &Url) -> anyhow::Result<()> {
-        Ok(webbrowser::open(url)?) //AsRef<str>
+        Ok(webbrowser::open(url)?) // Deref coercion
+        //If T implements Deref<Target = U>, and v is a value of type T, then:
+        //    In immutable contexts, *v (where T is neither a reference nor a raw pointer) is equivalent to *Deref::deref(&v).
+        //    Values of type &T are coerced to values of type &U
+        //    T implicitly implements all the methods of the type U which take the &self receiver.
     }
 }
 
