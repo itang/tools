d16f6aedcc4151d8bd882937ee82258d812001c4
itang <live.tang@gmail.com>
2024-07-20 08:31:14 +0800

    chore: u

--- title/rtitle/src/app.rs
+++ title/rtitle/src/app.rs
@@ -1,25 +1,63 @@
-use chrono::{DateTime, Local};
+use rtitle::ExtractError;
 
-use rtitle::{ExtractError, Title};
+use self::handlers::*;
 
 const VERSION: &str = "0.3.2-20240719.01";
 
-pub(crate) fn print_version() {
-    println!("rtitle-v{VERSION}");
+/// The router
+pub struct Router {
+    args: Vec<String>,
 }
 
-pub(crate) fn print_help() {
-    println!("e.g.\n  rtitle <url>")
+impl Router {
+    /// create a new router
+    pub fn new(args: Vec<String>) -> Self {
+        Self { args }
+    }
+
+    /// run the router
+    pub async fn run(&self) -> Result<(), ExtractError> {
+        match &self.args[..] {
+            [] => {
+                println!("Please input the url.");
+                print_help();
+            },
+            [head, ..] if head == "-h" || head == "--help" => print_help(),
+            [head, ..] if head == "-v" || head == "--version" => print_version(),
+            urls => {
+                for url in urls {
+                    print_title(url).await?;
+                }
+            },
+        }
+
+        Ok(())
+    }
 }
 
-///print title of the url
-pub(crate) async fn print_title(url: &str) -> Result<(), ExtractError> {
-    let title = Title::extract_from_url(url).await?;
+mod handlers {
+    use chrono::{DateTime, Local};
+    use rtitle::{ExtractError, Title};
 
-    let local: DateTime<Local> = Local::now();
-    let now = local.format("%Y-%m-%d %H:%M");
+    use super::VERSION;
 
-    println!("\nrs << Read.new \"{url}\",\n  title: \"{}\",\n  created_at: \"{now}\"\n", title);
+    pub(crate) fn print_version() {
+        println!("rtitle-v{VERSION}");
+    }
 
-    Ok(())
+    pub(crate) fn print_help() {
+        println!("e.g.\n  rtitle <url>")
+    }
+
+    ///print title of the url
+    pub(crate) async fn print_title(url: &str) -> Result<(), ExtractError> {
+        let title = Title::extract_from_url(url).await?;
+
+        let local: DateTime<Local> = Local::now();
+        let now = local.format("%Y-%m-%d %H:%M");
+
+        println!("\nrs << Read.new \"{url}\",\n  title: \"{}\",\n  created_at: \"{now}\"\n", title);
+
+        Ok(())
+    }
 }
--- title/rtitle/src/main.rs
+++ title/rtitle/src/main.rs
@@ -1,26 +1,17 @@
 #![deny(clippy::unwrap_used)]
 #![forbid(unsafe_code)]
 
+mod app;
+
 use std::env;
 
-mod app;
+use self::app::Router;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
-    let args: Vec<String> = env::args().skip(1).collect();
-    match &args[..] {
-        [] => {
-            println!("Please input the url.");
-            app::print_help();
-        },
-        [head, ..] if head == "-h" || head == "--help" => app::print_help(),
-        [head, ..] if head == "-v" || head == "--version" => app::print_version(),
-        urls => {
-            for url in urls {
-                app::print_title(url).await?;
-            }
-        },
-    }
+    let args = env::args().skip(1).collect::<Vec<_>>();
+
+    Router::new(args).run().await?;
 
     Ok(())
 }
