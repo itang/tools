afb6b88a25fb0b2976299fb74d3b62f8300ebe9b
itang <live.tang@gmail.com>
2024-07-27 13:34:16 +0800

    chore: u

--- file_counter/file_counter_rs/src/lib/files.rs
+++ file_counter/file_counter_rs/src/lib/files.rs
@@ -3,12 +3,14 @@ use std::path::{Path, PathBuf};
 
 use anyhow::Result;
 
+type PredicatePathFn = dyn Fn(&Path) -> bool;
+
 /// get all files in some dir with predicate
-pub fn files<P>(root_dir: P, predicate: Box<dyn Fn(&Path) -> bool>) -> Result<Vec<PathBuf>>
+pub fn files<P>(root_dir: P, predicate: Box<PredicatePathFn>) -> Result<Vec<PathBuf>>
 where
     P: AsRef<Path>,
 {
-    fn _rec_files<S: AsRef<Path>>(p: S, pred: &dyn Fn(&Path) -> bool, res: &mut Vec<PathBuf>) -> Result<()> {
+    fn _rec_files<S: AsRef<Path>>(p: S, pred: &PredicatePathFn, res: &mut Vec<PathBuf>) -> Result<()> {
         let files = fs::read_dir(p)?;
         for entry in files {
             let entry = entry?;
