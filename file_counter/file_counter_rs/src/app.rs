400eb4e83ae5d4e92e8d93ab631658a18aeeaebf
itang <live.tang@gmail.com>
2024-07-30 10:24:00 +0800

    chore: u

--- file_counter/file_counter_rs/src/app.rs
+++ file_counter/file_counter_rs/src/app.rs
@@ -13,9 +13,12 @@ pub struct Args {
     #[arg(short, long)]
     contains: Option<Vec<String>>,
 
-    ///The dir
+    ///The source dir
     #[arg(short, long, default_value = ".")]
     dir: PathBuf,
+
+    #[arg(long)]
+    exported_dir: Option<PathBuf>,
 }
 
 impl Args {
@@ -36,21 +39,58 @@ impl Router {
 
     pub fn run(self) -> anyhow::Result<()> {
         println!("DEBUG: args: {:?}", self.args);
-        handlers::do_files(self.args)
+        handlers::main(self.args)
     }
 }
 
 mod handlers {
+    use super::Args;
+    use anyhow::anyhow;
+    use ifile_counter::{build_predicate_contains_fn, build_predicate_ext_fn, PredicatePathFn};
     use std::path::{Path, PathBuf};
 
-    use super::Args;
-    use ifile_counter::{build_predicate_contains_fn, build_predicate_ext_fn, PredicatePathFn};
-
-    pub(super) fn do_files(args: Args) -> anyhow::Result<()> {
+    pub(super) fn main(args: Args) -> anyhow::Result<()> {
         let files = ifile_counter::files(args.dir.clone(), Box::new(build_predicate_fn(args.clone())))?;
+
         output_format(&files);
 
+        if let Some(exported_dir) = &args.exported_dir {
+            println!("INFO: export matched files to directory {:?}", exported_dir);
+            export(&files, exported_dir)?
+        }
+
         Ok(())
+    }
+
+    fn export(files: &[PathBuf], exported_dir: &PathBuf) -> anyhow::Result<()> {
+        if exported_dir.exists() {
+            return Err(anyhow!("{:?} already exists", exported_dir));
+        } else {
+            std::fs::create_dir_all(exported_dir)?;
+        }
+
+        for file in files {
+            let mut new_file = PathBuf::from(exported_dir);
+            let relative_path = get_relative_path(exported_dir, file)?;
+
+            new_file.push(relative_path);
+
+            if let Some(p) = new_file.parent() {
+                if !p.exists() {
+                    std::fs::create_dir_all(p)?;
+                }
+            }
+
+            std::fs::copy(file, &new_file)?;
+            println!("DEBUG: copy {:?}", &new_file);
+        }
+
+        Ok(())
+    }
+
+    fn get_relative_path(_dir: &Path, file: &Path) -> anyhow::Result<PathBuf> {
+        Ok(file.to_path_buf())
+        //Ok(file.strip_prefix(std::env::current_dir()?)?.to_path_buf())
     }
 
     fn build_predicate_fn(args: Args) -> impl Fn(&Path) -> bool {
