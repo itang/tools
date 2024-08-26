78649ff162fafec6a1e6b90e98c68d5d87bbc136
itang <live.tang@gmail.com>
2024-08-26 12:03:23 +0800

    chore: u

--- dict/rdict/src/lib/dict.rs
+++ dict/rdict/src/lib/dict.rs
@@ -1,6 +1,9 @@
-use crate::util;
 use std::error::Error;
 use std::fmt::{Display, Formatter};
+
+use scraper::{Html, Selector};
+
+use crate::util;
 
 ///Dict Result
 #[derive(Debug)]
@@ -25,24 +28,43 @@ impl Display for DictResult {
 pub async fn dict(word: &str) -> Result<DictResult, Box<dyn Error>> {
     // content: owned move ?
     //TODO: extract pronunciation
-    fn extract_result(mut content: String) -> Option<DictResult> {
-        if let Some(p1) = content.find("trans-container") {
-            content.drain(..p1);
-            if let Some(p2) = content.find("<li>") {
-                content.drain(..p2);
-                let (end, start) = (content.find("</li>").expect("find"), "</li>".len() - 1);
-                return Some(DictResult::new(content.drain(start..end).collect(), "".to_string()));
-            }
+    fn extract_result(content: &str) -> Option<DictResult> {
+        // let document = Html::parse_document(r#"<html><div class="trans-container"><span class="trans">你好</span></div></html>"#);
+        // dbg!(document.html());
+
+        let document = Html::parse_document(content);
+        // let html = document.html();
+        // let document = Html::parse_document(&html);
+
+        let span_trans_selector = Selector::parse("div.trans-container span.trans").expect("selector parse");
+
+        for element in document.select(&span_trans_selector) {
+            dbg!(&element.value().name());
         }
 
-        None
+        let values: Vec<String> = document
+            .select(&span_trans_selector)
+            .map(|e| {
+                dbg!(&e);
+                e.inner_html()
+            })
+            .collect();
+        let r = values.join(", ");
+        dbg!(&r);
+
+        let phonetic_selector = Selector::parse("span.phonetic").expect("selector parse");
+        let values: Vec<String> = document.select(&phonetic_selector).map(|e| e.inner_html()).collect();
+        let p = values.join(", ");
+        dbg!(&p);
+
+        Some(DictResult::new(r, p))
     }
 
     let url = format!("http://dict.youdao.com/search?q={}&keyfrom=dict.index" /*DICT_SERVICE_URL*/, word);
 
     util::http_get_as_string(&url)
         .await
-        .and_then(|content| extract_result(content).ok_or_else(|| From::from("无法解析获取翻译内容")))
+        .and_then(|content| extract_result(&content).ok_or_else(|| From::from("无法解析获取翻译内容")))
 }
 
 /// ////////////////////////////////////////////////////////////////////////////
