5765b10ecb067fc393bcb961f1eec297854ab4f2
itang <live.tang@gmail.com>
2024-08-26 22:27:05 +0800

    chore: u

--- dict/rdict/src/lib/dict.rs
+++ dict/rdict/src/lib/dict.rs
@@ -1,33 +1,21 @@
 use std::error::Error;
-use std::fmt::{Display, Formatter};
+
+use scraper::{ElementRef, Html, Selector};
 
 use crate::util;
-use scraper::{Html, Selector};
 
-//Tran Result
+///Tran Result
 #[derive(Debug)]
 pub struct TranResult {
     pub part_of_speech: String,
     pub explanation: String,
 }
 
-impl Display for TranResult {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}: {}", self.part_of_speech, self.explanation)
-    }
-}
-
-//Pronunciation Result
+///Pronunciation Result
 #[derive(Debug)]
 pub struct PronResult {
     pub alias: String,
     pub pronunciation: String,
-}
-
-impl Display for PronResult {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}: {}", self.alias, self.pronunciation)
-    }
 }
 
 ///Dict Result
@@ -39,40 +27,12 @@ pub struct DictResult {
 
 /// dict
 pub async fn dict(word: &str) -> Result<DictResult, Box<dyn Error>> {
-    //TODO: 优化抽取结果
     fn extract_result(content: &str) -> Option<DictResult> {
         let document = Html::parse_document(content);
 
-        let span_trans_selector = Selector::parse("div.trans-container li.word-exp").expect("selector parse");
-        let result: Vec<TranResult> = document
-            .select(&span_trans_selector)
-            .map(|e| {
-                let s1 = Selector::parse("span.pos").expect("selector parse");
-                let part_of_speech = e.select(&s1).map(|it| it.inner_html()).collect::<Vec<String>>().join("");
-                let s2 = Selector::parse("span.trans").expect("selector parse");
-                let explanation = e
-                    .select(&s2)
-                    .map(|it| it.inner_html())
-                    .collect::<Vec<String>>()
-                    .join("")
-                    .replace("&lt;", "<")
-                    .replace("&gt;", ">");
+        let result = parse_result(&document);
 
-                TranResult { part_of_speech, explanation }
-            })
-            .collect();
-
-        let phone_selector = Selector::parse("div.trans-container div.per-phone").expect("selector parse");
-        let pronunciation: Vec<PronResult> = document
-            .select(&phone_selector)
-            .map(|e| {
-                let s1 = Selector::parse(":first-child").expect("selector parse");
-                let alias = e.select(&s1).map(|it| it.inner_html()).collect::<Vec<String>>().join("");
-                let s2 = Selector::parse("span.phonetic").expect("selector parse");
-                let pronunciation = e.select(&s2).map(|it| it.inner_html()).collect::<Vec<String>>().join("");
-                PronResult { alias, pronunciation }
-            })
-            .collect();
+        let pronunciation = parse_pronunciation(&document);
 
         Some(DictResult { result, pronunciation })
     }
@@ -84,8 +44,70 @@ pub async fn dict(word: &str) -> Result<DictResult, Box<dyn Error>> {
         .and_then(|content| extract_result(&content).ok_or_else(|| From::from("无法解析获取翻译内容")))
 }
 
-/// ////////////////////////////////////////////////////////////////////////////
-#[tokio::test]
-async fn test_dict() {
-    assert!(dict("hello").await.expect("ok").result.iter().any(|it| it.explanation.contains("你好")));
+fn parse_to_string(element_ref: ElementRef, selector: &Selector) -> String {
+    element_ref.select(selector).map(|it| it.inner_html()).collect::<Vec<String>>().join("")
+}
+
+fn parse_and_wrap(element_ref: ElementRef, selector: &Selector) -> String {
+    parse_to_string(element_ref, selector).replace("&lt;", "<").replace("&gt;", ">")
+}
+
+fn parse_result(document: &Html) -> Vec<TranResult> {
+    let span_trans_selector = Selector::parse("div.trans-container li.word-exp").expect("selector parse");
+
+    let mut result: Vec<TranResult> = document
+        .select(&span_trans_selector)
+        .map(|e| {
+            let s1 = Selector::parse("span.pos").expect("selector parse");
+            let part_of_speech = parse_to_string(e, &s1);
+            let s2 = Selector::parse("span.trans").expect("selector parse");
+            let explanation = parse_and_wrap(e, &s2);
+
+            TranResult { part_of_speech, explanation }
+        })
+        .collect();
+
+    if result.is_empty() {
+        let span_trans_selector = Selector::parse("div.trans-container li.word-exp-ce").expect("selector parse");
+        result = document
+            .select(&span_trans_selector)
+            .map(|e| {
+                let s1 = Selector::parse("span.index").expect("selector parse");
+                let index = parse_to_string(e, &s1);
+                let s2 = Selector::parse("div.trans-ce a.point").expect("selector parse");
+                let word = parse_and_wrap(e, &s2);
+
+                let s3 = Selector::parse(".word-exp_tran").expect("selector parse");
+                let explanation = parse_to_string(e, &s3);
+                TranResult { part_of_speech: index + " " + &word, explanation }
+            })
+            .collect();
+    }
+
+    result
+}
+
+fn parse_pronunciation(document: &Html) -> Vec<PronResult> {
+    let phone_selector = Selector::parse("div.trans-container div.per-phone").expect("selector parse");
+    let pronunciation: Vec<PronResult> = document
+        .select(&phone_selector)
+        .map(|e| {
+            let s1 = Selector::parse(":first-child").expect("selector parse");
+            let alias = parse_to_string(e, &s1);
+            let s2 = Selector::parse("span.phonetic").expect("selector parse");
+            let pronunciation = parse_and_wrap(e, &s2);
+            PronResult { alias, pronunciation }
+        })
+        .collect();
+    pronunciation
+}
+
+#[cfg(test)]
+mod tests {
+    use super::dict;
+
+    #[tokio::test]
+    async fn test_dict() {
+        assert!(dict("hello").await.expect("ok").result.iter().any(|it| it.explanation.contains("你好")));
+    }
 }
