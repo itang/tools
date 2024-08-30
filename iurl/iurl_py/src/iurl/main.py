5b1a9d13426be0df9000379d07f863ec4d4dae54
itang <live.tang@gmail.com>
2024-08-30 10:21:36 +0800

    chore: u

--- iurl/iurl_py/src/iurl/main.py
+++ iurl/iurl_py/src/iurl/main.py
@@ -1,13 +1,15 @@
-from urllib.parse import urlparse, parse_qsl, parse_qs
 import json
 import argparse
+from urllib.parse import ParseResult, urlparse, parse_qsl, parse_qs
+from termcolor import colored
+
+
+DEFAULT_URL = "http://39.104.227.186:8089/api/api/runtime/organization/department/tree?deptIds=&filterType=root_display&sourceType=portal&corpId=&excludeCorpId=&selectUserIds=&workflowInstanceId=&activityCode=&formRootDeptIds=&queryType="
 
 
 def _get_args() -> argparse.Namespace:
-    link = "http://39.104.227.186:8089/api/api/runtime/organization/department/tree?deptIds=&filterType=root_display&sourceType=portal&corpId=&excludeCorpId=&selectUserIds=&workflowInstanceId=&activityCode=&formRootDeptIds=&queryType="
-
     parser = argparse.ArgumentParser(description="My url pretty parser")
-    parser.add_argument("--url", type=str, help="输入url", default=link)
+    parser.add_argument("--url", type=str, help="输入url", default=DEFAULT_URL)
     parser.add_argument(
         "urls_args",
         type=str,
@@ -28,21 +30,23 @@ def _get_args() -> argparse.Namespace:
 def _main(args: argparse.Namespace) -> None:
     urls = args.urls_args or [args.url]
     for index, url in enumerate(urls):
-        url_obj = urlparse(url)
+        url_obj: ParseResult = urlparse(url)
 
         parse_query_fn = parse_qsl if args.query_model == "qsl" else parse_qs
 
         url_dict = {
             "scheme": url_obj.scheme,
             "hostname": url_obj.hostname,
+            "port": url_obj.port,
             "path": url_obj.path,
             "query": dict(parse_query_fn(url_obj.query, keep_blank_values=True)),
             "fragment": url_obj.fragment,
         }
 
-        print(f"{index+1}: {url}")
-        print(">>>>")
-        print(json.dumps(url_dict, indent=4, ensure_ascii=False))
+        print(colored(f"{index+1}: {url}", "blue"))
+        print(colored("\n>>>>\n", "light_grey"))
+        print(colored(json.dumps(url_dict, indent=4, ensure_ascii=False), "green"))
+        print()
 
 
 def main() -> None:
