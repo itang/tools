fec6c1989df2c4f0dd2fbc5c01e14456632cc7eb
itang <live.tang@gmail.com>
2024-09-06 16:35:09 +0800

    chore: u

--- idemo/idemo_py/src/idemo/app.py
+++ idemo/idemo_py/src/idemo/app.py
@@ -9,31 +9,10 @@ from selenium.webdriver.remote.webelement import WebElement
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-__all__ = ["MyDriver", "get_driver"]
+__all__ = ["MyDriver", "create_driver"]
 
 
-def _try_get_element(driver: WebDriver, css_selector: str) -> WebElement | None:
-    try:
-        return driver.find_element(By.CSS_SELECTOR, css_selector)
-    except:  # noqa: E722
-        return None
-
-
-def _get_button_or_wait(driver, wait_time: int = 2) -> WebElement:
-    count = 0
-    button = None
-
-    while not button:
-        # 等待元素加载
-        driver.implicitly_wait(wait_time)
-        print(f"INFO: [{count}] try get button...")
-        button = _try_get_element(driver, css_selector=".login-btn")
-        count += 1
-
-    return button
-
-
-def get_driver() -> WebDriver:
+def create_driver(maximize_window: bool = True) -> WebDriver:
     # 设置WebDriver
     chrome_options = Options()
     # 尝试添加这些参数来隐藏被控制提示
@@ -41,17 +20,26 @@ def get_driver() -> WebDriver:
     chrome_options.add_experimental_option("detach", True)
     chrome_options.add_argument("--disable-infobars")
 
-    return webdriver.Chrome(
+    driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()), options=chrome_options
     )
+
+    if maximize_window:
+        # 最大化浏览器窗口
+        driver.maximize_window()
+
+    return driver
 
 
 class MyDriver:
     def __init__(self, driver: WebDriver):
         self.driver = driver
 
-    def auto_login(self) -> None:
-        button = _get_button_or_wait(self.driver)
+    def auto_login(self, login_page: str) -> None:
+        # 打开网页
+        self.driver.get(login_page)
+
+        button = self._get_submit_button_or_wait()
 
         # 找到表单输入元素并填充数据
         username_input = self.driver.find_element(By.CSS_SELECTOR, ".login-input")
@@ -70,12 +58,31 @@ class MyDriver:
 
         print("INFO: Form submitted successfully.")
 
-    def wait_browser_closed(self) -> None:
+    def wait_browser_closed(self, try_interval_seconds: int = 1) -> None:
         while True:
             try:
                 # 尝试获取窗口标题，如果窗口已关闭，将引发NoSuchWindowException
                 self.driver.title
-                time.sleep(1)  # 稍等一段时间再次检查
+                time.sleep(try_interval_seconds)  # 稍等一段时间再次检查
             except NoSuchWindowException:
                 print("INFO: 窗口已关闭, 自动退出.")
                 break
+
+    def _try_get_element(self, css_selector: str) -> WebElement | None:
+        try:
+            return self.driver.find_element(By.CSS_SELECTOR, css_selector)
+        except:  # noqa: E722
+            return None
+
+    def _get_submit_button_or_wait(self, wait_time_seconds: int = 2) -> WebElement:
+        count = 0
+        button = None
+
+        while not button:
+            # 等待元素加载
+            self.driver.implicitly_wait(wait_time_seconds)
+            print(f"INFO: [{count}] try get submit button...")
+            button = self._try_get_element(css_selector=".login-btn")
+            count += 1
+
+        return button
--- idemo/idemo_py/src/idemo/main.py
+++ idemo/idemo_py/src/idemo/main.py
@@ -1,22 +1,16 @@
-from .app import get_driver, MyDriver
+from .app import create_driver, MyDriver
 
 
 def main() -> None:
-    driver = get_driver()
+    driver = create_driver(maximize_window=True)
 
     try:
-        page = "http://39.104.227.186:8089/login?corpId=dingc55494e63f745d5335c2f4657eb6378f"
-
-        # 打开网页
-        driver.get(page)
-
-        # 最大化浏览器窗口
-        driver.maximize_window()
-
         my_driver = MyDriver(driver)
 
         # 调用自动登录函数
-        my_driver.auto_login()
+        my_driver.auto_login(
+            login_page="http://39.104.227.186:8089/login?corpId=dingc55494e63f745d5335c2f4657eb6378f"
+        )
 
         # 等待浏览器关闭
         my_driver.wait_browser_closed()
