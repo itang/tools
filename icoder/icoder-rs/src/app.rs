d9dde4d6dda29361580b3a6d765136e8397248c7
itang <live.tang@gmail.com>
2024-11-29 22:48:56 +0800

    chore: add num command for(显示10、16、8、2进制)

--- icoder/icoder-rs/src/app.rs
+++ icoder/icoder-rs/src/app.rs
@@ -22,10 +22,10 @@ pub enum Command {
     Base64(CoderOptions),
     ///hex
     Hex(CoderOptions),
-    ///i2hex
-    I2hex(CoderOptions),
-    ///i2binary
-    I2binary(CoderOptions),
+    // ///i2hex
+    // I2hex(CoderOptions),
+    // ///i2binary
+    // I2binary(CoderOptions),
     ///upcase
     Upcase(UpcaseOptions),
     ///lowcase
@@ -44,8 +44,10 @@ pub enum Command {
     Split(SplitOptions),
     ///delay
     Delay(DelayOptions),
-    ///Hexs
-    Hexs(HexsOptions),
+    ///Num
+    Num(NumOptions),
+    ///Nums
+    Nums(NumsOptions),
     ///Pows
     Pows(PowsOptions),
 }
@@ -124,13 +126,18 @@ pub struct DelayOptions {
     ///input
     pub input: Option<u32>,
 }
-///hexs
+///nums
 #[derive(Args, Debug, Clone)]
-pub struct HexsOptions {}
+pub struct NumsOptions {}
 
 ///pows
 #[derive(Args, Debug, Clone)]
 pub struct PowsOptions {}
+
+#[derive(Args, Debug, Clone)]
+pub struct NumOptions {
+    pub input: Option<String>,
+}
 
 ///IRouter
 pub trait IRouter {
@@ -162,20 +169,20 @@ impl IRouter for Router {
                     Hex.encode(options.input.or_read_line())?
                 }
             },
-            Command::I2hex(options) => {
-                if options.decode {
-                    I2Hex.decode(options.input.or_read_line())?
-                } else {
-                    I2Hex.encode(options.input.or_read_line())?
-                }
-            },
-            Command::I2binary(options) => {
-                if options.decode {
-                    I2Binary.decode(options.input.or_read_line())?
-                } else {
-                    I2Binary.encode(options.input.or_read_line())?
-                }
-            },
+            // Command::I2hex(options) => {
+            //     if options.decode {
+            //         I2Hex.decode(options.input.or_read_line())?
+            //     } else {
+            //         I2Hex.encode(options.input.or_read_line())?
+            //     }
+            // },
+            // Command::I2binary(options) => {
+            //     if options.decode {
+            //         I2Binary.decode(options.input.or_read_line())?
+            //     } else {
+            //         I2Binary.encode(options.input.or_read_line())?
+            //     }
+            // },
             Command::Uuid(options) => uuid(options.upcase, options.no_underline),
             Command::Upcase(options) => options.input.or_read_line().to_uppercase(),
             Command::Lowcase(options) => options.input.or_read_line().to_lowercase(),
@@ -200,7 +207,13 @@ impl IRouter for Router {
 
                 String::default()
             },
-            Command::Hexs(_options) => {
+            Command::Num(options) => {
+                let raw_s = options.input.or_read_line();
+                let value = num_from_string(&raw_s)?;
+
+                to_num_string(&raw_s, value)
+            },
+            Command::Nums(_options) => {
                 let ds = [
                     "0xFFFF",
                     "0x8000",
@@ -223,31 +236,17 @@ impl IRouter for Router {
                 ];
                 ds.into_iter()
                     .map(|raw_s| {
-                        let value = match raw_s {
-                            hex_string if hex_string.starts_with("0x") || hex_string.starts_with("0X") => {
-                                let trimmed = &hex_string[2..];
-                                u32::from_str_radix(trimmed, 16).expect("Invalid hex string")
-                            },
-                            binary_string if binary_string.starts_with("0b") || binary_string.starts_with("0b") => {
-                                let trimmed = &binary_string[2..];
-                                u32::from_str_radix(trimmed, 2).expect("Invalid binary string")
-                            },
-                            _ => {
-                                let trimmed = raw_s;
-                                u32::from_str_radix(trimmed, 10).expect("Invalid decimal string")
-                            },
-                        };
-
-                        format!("{:12}: {}", raw_s, value)
+                        let value = num_from_string(raw_s).expect("failed to parse num");
+                        to_num_string(raw_s, value)
                     })
                     .collect::<Vec<String>>()
                     .join("\n")
             },
             Command::Pows(_options) => (0..20)
                 .map(|i| {
-                    let value = u32::pow(2, i);
+                    let value = u64::pow(2, i);
                     let s = format!("pow(2, {})", i);
-                    format!("{:<12} = {:<8} 0x{:<8x} 0b{:b}", s, value, value, value)
+                    to_num_string(&s, value)
                 })
                 .collect::<Vec<String>>()
                 .join("\n"),
@@ -257,4 +256,29 @@ impl IRouter for Router {
 
         Ok(())
     }
+}
+
+fn num_from_string(raw_s: &str) -> Result<u64, Box<dyn std::error::Error>> {
+    Ok(match raw_s {
+        hex_string if hex_string.starts_with("0x") || hex_string.starts_with("0X") => {
+            let trimmed = &hex_string[2..];
+            u64::from_str_radix(trimmed, 16)?
+        },
+        binary_string if binary_string.starts_with("0o") || binary_string.starts_with("0o") => {
+            let trimmed = &binary_string[2..];
+            u64::from_str_radix(trimmed, 8)?
+        },
+        binary_string if binary_string.starts_with("0b") || binary_string.starts_with("0b") => {
+            let trimmed = &binary_string[2..];
+            u64::from_str_radix(trimmed, 2)?
+        },
+        _ => {
+            let trimmed = raw_s;
+            u64::from_str_radix(trimmed, 10)?
+        },
+    })
+}
+
+fn to_num_string(raw_s: &str, value: u64) -> String {
+    format!("{:<12} = {:<8} 0x{:<8x} 0o{:<10o} 0b{:b}", raw_s, value, value, value, value)
 }
